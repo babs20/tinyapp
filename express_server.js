a8abdee00cfeba795625b7feaef8458e8afc76cb
Brady <thevillemc@gmail.com>
2021-01-18 19:35:24 +0000

    added urls_shows.ejs

--- express_server.js
+++ express_server.js
@@ -11,7 +11,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,6 +26,11 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
