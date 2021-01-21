f51208766405321a53124a76e4fb7d4b0f14ab0c
Brady <thevillemc@gmail.com>
2021-01-21 14:46:44 +0000

    final bug fixes and refactoring

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
-/* eslint-disable camelcase */
 'use strict';
+
 // IMPORT MODULES //
 const express = require("express");
 const bodyParser = require('body-parser');
@@ -21,13 +21,14 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 /// DATABASES ///
-const urlDatabase = {
+const urlDatabase = { // Simple Data For Testing Edge Cases
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
     userId: 'user1x1gd1',
     traffic: 0,
     unique: 0,
-    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }]
+    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }],
+    created: 1611191758000
   },
   "b2xabc": {
     longURL: "http://www.lighthouselabs.ca",
@@ -35,14 +36,16 @@ const urlDatabase = {
     dateCreated: 1611191758000,
     traffic: 0,
     unique: 0,
-    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }]
+    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }],
+    created: 1611191758000
   },
   "9sm5xK": {
     longURL: "http://www.google.com",
     userId: 'user1x1gd1',
     traffic: 0,
     unique: 0,
-    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }]
+    visitLog: [{ timestamp: 1611191758000, visitorId: 'user' }],
+    created: 1611191758000
   }
 };
 
@@ -52,7 +55,7 @@ const users = {};
 
 // ROOT //
 app.get("/", (req, res) => { // Redirect to URLS or Login depending on user login
-  if (!req.session.user_id) {
+  if (!req.session.clientId) {
     res.redirect('/login');
   } else {
     res.redirect('/urls');
@@ -61,29 +64,32 @@ app.get("/", (req, res) => { // Redirect to URLS or Login depending on user logi
 
 // URLS //
 app.get('/urls', (req, res) => { // CREATE MY URLS PAGE
-  const userId = findUserId(req.session.user_id, users);
+  const userId = findUserId(req.session.clientId, users);
   const templateVars = { urls: urlsForUser(userId, urlDatabase), userId: userId };
   res.render('urls_index', templateVars);
 });
 
 app.delete('/urls/:shortURL', (req, res) => { // DELETE LINK FROM DATABASE
-  const userId = findUserId(req.session.user_id, users);
-  let templateVars = {};
-  if (!urlDatabase[req.params.shortURL]) {
-    templateVars = { userId, errMessage: 'Please log in to delete this file.' };
+  const userId = findUserId(req.session.clientId, users);
+  let templateVars = { userId };
+
+  if (!urlDatabase[req.params.shortURL]) { // Link doesn't exist
+    templateVars['errMessage'] = '404: The link you are trying to delete does not exist.';
     return res.render('urls_error', templateVars);
-  } else if (req.session.user_id === urlDatabase[req.params.shortURL].userId) {
+  } else if (req.session.clientId !== urlDatabase[req.params.shortURL].userId) { // Wrong Account / Not Logged In
+    templateVars['errMessage'] = '403: You do not have permission to delete this file. If this is your link, please log in.';
+    res.render('urls_error', templateVars);
+  } else {
     delete urlDatabase[req.params.shortURL];
+    return res.redirect('/urls');
   }
-  templateVars = { userId, errMessage: 'You do not have permission to delete this file' };
-  res.render('urls_error', templateVars);
 });
 
 app.put('/urls/:id', (req, res) => { // UPDATE LINK AFTER EDIT
   const id = Object.keys(req.body)[0];
   if (!urlDatabase[id]) { // Not logged in
     return res.redirect('/login');
-  } else if (req.session.user_id !== urlDatabase[id].userId) { // Not the right user
+  } else if (req.session.clientId !== urlDatabase[id].userId) { // Not the right user
     return res.redirect(`/urls/${id}`);
   } else {
     urlDatabase[id].longURL = req.body[id];
@@ -95,7 +101,7 @@ app.post('/urls', (req, res) => { // CREATE NEW SHORT LINK AND ADD TO DATABASE
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     longURL: req.body.longURL,
-    userId: req.session.user_id,
+    userId: req.session.clientId,
     traffic: 0,
     unique: 0,
     created: Date.now(),
@@ -105,7 +111,7 @@ app.post('/urls', (req, res) => { // CREATE NEW SHORT LINK AND ADD TO DATABASE
 });
 
 app.get('/urls/new', (req, res) => { // CREATE NEW SHORT URL
-  const userId = findUserId(req.session.user_id, users);
+  const userId = findUserId(req.session.clientId, users);
   if (userId === undefined) {
     res.redirect('/login');
   } else {
@@ -120,7 +126,7 @@ app.get('/urls/:id', (req, res) => { // URL SHOW PAGE
     urls: urlDatabase,
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id] ? urlDatabase[req.params.id].longURL : null,
-    userId: findUserId(req.session.user_id, users),
+    userId: findUserId(req.session.clientId, users),
     linkExists,
   };
   res.render('urls_show', templateVars);
@@ -129,7 +135,7 @@ app.get('/urls/:id', (req, res) => { // URL SHOW PAGE
 // REDIRECT SHORTEN LINKS //
 app.get('/u/:id', (req, res) => {
   const shortURL = req.params.id;
-  if (urlDatabase[shortURL]) {
+  if (urlDatabase[shortURL]) { // DEAL WITH ANALYTICS BEFORE REDIRECT
     // VISITS
     let viewCookieExists = false;
     const idObject = urlDatabase[shortURL];
@@ -141,8 +147,9 @@ app.get('/u/:id', (req, res) => {
     urlDatabase[shortURL].visitLog.push({ timestamp: Date.now(), visitorId: req.session.visitorId });
 
     return res.redirect(urlDatabase[shortURL].longURL);
+
   } else {
-    const userId = findUserId(req.session.user_id, users);
+    const userId = findUserId(req.session.clientId, users);
     const templateVars = { errMessage: '404. That link does not exist, please check your url.', userId };
     res.status(404).render('urls_error', templateVars);
   }
@@ -150,13 +157,13 @@ app.get('/u/:id', (req, res) => {
 
 // LOGIN //
 app.post('/login', (req, res) => {
-  const userId = findUserId(req.session.user_id, users);
+  const userId = findUserId(req.session.clientId, users);
   const reqEmail = req.body.email;
   const reqPass = req.body.password;
 
-  passwordChecker(reqEmail, reqPass, users, (bool, id) => {
-    if (bool) {
-      req.session.user_id = id;
+  passwordChecker(reqEmail, reqPass, users, (correctLogin, id) => {
+    if (correctLogin) {
+      req.session.clientId = id;
       return res.redirect('/urls');
     } else {
       const templateVars = { userId, errMessage: 'The log in information is incorrect please try again.' };
@@ -166,19 +173,19 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  if (req.session.user_id) {
+  if (req.session.clientId) {
     return res.redirect('/urls');
   }
-  const templateVars = { userId: findUserId(req.session.user_id, users) };
+  const templateVars = { userId: findUserId(req.session.clientId, users) };
   res.render('urls_login', templateVars);
 });
 
 // REGISTER //
 app.get('/register', (req, res) => {
-  if (req.session.user_id) {
+  if (req.session.clientId) {
     return res.redirect('/urls');
   }
-  const templateVars = { userId: findUserId(req.session.user_id, users), errMessage: '404' };
+  const templateVars = { userId: findUserId(req.session.clientId, users), errMessage: '404' };
   res.render('urls_register', templateVars);
 });
 
@@ -186,17 +193,21 @@ app.post('/register', (req, res) => {
   const reqEmail = req.body.email;
   const reqPass = req.body.password;
   const hashedPass = bcrypt.hashSync(reqPass, 10);
-  if ((reqEmail === '' || reqPass === '') || getUserByEmail(reqEmail, users, (bool) => bool)) {
-    const templateVars = {
-      userId: findUserId(req.session.user_id, users),
-      errMessage: 'That email is already in use. Try to log In.'
-    };
+  const templateVars = { userId: findUserId(req.session.clientId, users) };
+
+  if ((reqEmail === '' || reqPass === '')) { // Both fields empty
+    templateVars['errMessage'] = 'Please enter information into the text fields.';
     return res.render('urls_error', templateVars);
+  } else if (getUserByEmail(reqEmail, users, (bool) => bool)) { // Email in use
+    templateVars['errMessage'] = 'That E-Mail is already in use.';
+    return res.render('urls_error', templateVars);
+  } else {
+    const ranUserId = `user${generateRandomString()}`;
+    users[ranUserId] = { id: ranUserId, email: reqEmail, password: hashedPass };
+    req.session.clientId = ranUserId;
+    res.redirect('/urls');
   }
-  const ranUserId = `user${generateRandomString()}`;
-  users[ranUserId] = { id: ranUserId, email: reqEmail, password: hashedPass };
-  req.session.user_id = ranUserId;
-  res.redirect('/urls');
+
 });
 
 // LOGOUT //
